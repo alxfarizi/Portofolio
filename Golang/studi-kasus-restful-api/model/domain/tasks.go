ebea8eb75fe15ab6806c0176b7f466b52752a184
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2026-01-14 11:48:16 +0700

    fix: perbaikan model task

--- Golang/studi-kasus-restful-api/model/domain/tasks.go
+++ Golang/studi-kasus-restful-api/model/domain/tasks.go
@@ -1,1 +1,19 @@
 package domain
+
+import "time"
+
+type TaskStatus string
+
+const (
+	StatusPending    TaskStatus = "PENDING"
+	StatusInProgress TaskStatus = "IN_PROGRESS"
+	StatusCompleted  TaskStatus = "COMPLETED"
+)
+
+type Task struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Status      TaskStatus `json:"status"`
+	Deadline    time.Time  `json:"deadline"`
+}
