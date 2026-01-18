1a899285b9f51826bcf33695841d7f691bf25805
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2026-01-18 14:53:35 +0700

    refactor: improve task controller implementation

--- Golang/studi-kasus-restful-api/controller/task_controller_impl.go
+++ Golang/studi-kasus-restful-api/controller/task_controller_impl.go
@@ -2,6 +2,9 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
+	"studi-kasus-restful-api/helper"
+	"studi-kasus-restful-api/model/web"
 	"studi-kasus-restful-api/service"
 
 	"github.com/julienschmidt/httprouter"
@@ -12,26 +15,75 @@ type TaskControllerImpl struct {
 }
 
 func (controller *TaskControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	//TODO implement me
-	panic("implement me")
+	taskCreateRequest := web.TaskCreateRequest{}
+	helper.ReadFromRequestBody(request, &taskCreateRequest)
+
+	taskResponse := controller.TaskService.Create(request.Context(), taskCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   taskResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
 
 func (controller *TaskControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	//TODO implement me
-	panic("implement me")
+	taskUpdateRequest := web.TaskUpdateRequest{}
+	helper.ReadFromRequestBody(request, &taskUpdateRequest)
+
+	taskId := params.ByName("id")
+	id, err := strconv.Atoi(taskId)
+	helper.PanicIfError(err)
+
+	taskUpdateRequest.Id = id
+
+	taskResponse := controller.TaskService.Update(request.Context(), taskUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   taskResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
 
 func (controller *TaskControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	//TODO implement me
-	panic("implement me")
+	taskId := params.ByName("id")
+	id, err := strconv.Atoi(taskId)
+	helper.PanicIfError(err)
+
+	controller.TaskService.Delete(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
 
 func (controller *TaskControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	//TODO implement me
-	panic("implement me")
+	taskId := params.ByName("id")
+	id, err := strconv.Atoi(taskId)
+	helper.PanicIfError(err)
+
+	taskResponse := controller.TaskService.FindById(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   taskResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
 
 func (controller *TaskControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	//TODO implement me
-	panic("implement me")
+	taskResponses := controller.TaskService.FindAll(request.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   taskResponses,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
